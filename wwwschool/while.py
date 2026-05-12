0cbae9a9fd9535d4bfb473bd206b64253d4a649f
MerinaGadde <merinagadde2611@gmail.com>
2026-05-12 10:23:33 +0530

    d

---
+++ wwwschool/while.py
@@ -0,0 +1,46 @@
+
+
+
+
+print("hello world")
+
+
+i=0
+
+while (i<=5):
+    print(i,end=" ")
+    i+=1
+print()
+
+print("*"*80)
+
+i=5
+while(i>0):
+    print(i,end=" ")
+    i-=1
+
+# 
+num=5
+
+
+
+
+
+
+
+
+
+i=1
+while (i<=10):
+    
+    print(f"{num}*{i}={num*i} ")
+    i+=1
+
+
+
+print("*"*50)
+
+
+
+
+
