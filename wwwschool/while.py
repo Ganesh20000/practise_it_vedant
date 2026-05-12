a949d1c2a4d0f4bcbfddd5f8daca34c4df4e3915
MerinaGadde <merinagadde2611@gmail.com>
2026-05-12 11:06:47 +0530

    done

--- wwwschool/while.py
+++ wwwschool/while.py
@@ -41,6 +41,76 @@ while (i<=10):
 print("*"*50)
 
 
+i=5;
+
+# while (i>0):
+    # rem=i%10
+    # print(rem)
+    # i=i//10
+    # print(i)
+    
+
+#^ reverse a noo
+
+num=45646466
 
 
 
+rev=0
+
+while(num>0):
+    rem=num%10
+    rev=rev*10+rem
+    num//=10
+
+
+print(rev)
+
+
+
+
+
+#palindrom of a no
+no=14141414141
+temp=no
+rev=0
+
+while(no>0):
+    rem=no%10
+    rev=rev*10+rem
+    no=no//10
+
+print(rev)
+
+if temp==rev:
+    print(" palindrom number*")
+else:
+    print("not a palindrome")
+
+
+
+
+
+
+# loop control statement
+
+
+
+for i in range(6):
+    i+=1
+    if(i==4):
+        continue
+    print(f"i is {i} " )
+
+
+# break
+
+
+print("*"*50)
+print()
+i=0
+while(i<=10):
+    i+=1
+    if i==5:
+        break
+    print(f"i is {i}")
