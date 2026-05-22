cf5cc1057b79928cf841ed81261c4e9adbcab6ed
Ganesh20000 <ganeshkarli19@gmail.com>
2026-05-22 23:34:41 +0530

    done

--- oops/oops.py
+++ oops/oops.py
@@ -3,6 +3,7 @@ class deg:
     # special method or magic method or dunder method
     # called as constructor
     def __init__(self):
+        print(id(self))
         self.id=5
         self.age=78
         self.name='alice'
@@ -10,14 +11,29 @@ class deg:
     def second(self,score):
         print(f"i am a alice i score {score} ")
 
+    
+
 
 # creating a obj or insteance of classs
+# we are priting id of self 
 op=deg()
+
+od=deg()
+
+# if id change then self also get change 
+print(id(op))
+
+
+print(id(od))
 
 op.second(7)
 # calling method in this class
 
 
 print(op.name)
+
+
+o=deg()
+print(o)
 
 
